6bdfc253d25fcbd738c4618ed2dd1d79c3e045da
Alex Memory <alexmemory@gmail.com>
2012-03-11 13:14:01 -0400

    run fast downward script

---
+++ pddl/fd_run.sh
@@ -0,0 +1,19 @@
+if [ -z "$FAST_DOWNWARD_HOME" ]; then
+    echo "FAST_DOWNWARD_HOME not set";
+    exit -1
+fi
+    
+if [ -z "$2" ]; then
+    echo "usage fd_run.sh <domain pddl file> <problem pddl file>";
+    exit -1
+fi
+
+cp $1 $FAST_DOWNWARD_HOME/src/tmp-domain.pddl
+cp $2 $FAST_DOWNWARD_HOME/src/tmp-problem.pddl
+cd $FAST_DOWNWARD_HOME/src
+rm output*
+rm sas_plan
+translate/translate.py tmp-domain.pddl tmp-problem.pddl
+preprocess/preprocess < output.sas
+search/downward --search "astar(blind())" < output
+cd -
